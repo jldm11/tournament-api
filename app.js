b01e1d942b37a0278c4ecd6f3e50f4f9f92d0461
jldm11 <isc.joseluisdiaz@gmail.com>
2018-02-11 23:18:32 -0600

    Test commit

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var config = {
 SwaggerExpress.create(config, function (err, swaggerExpress) {
   if (err) { throw err }
 
-  // install middleware
   swaggerExpress.register(app)
 
   var port = process.env.PORT || 10010
